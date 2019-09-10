fa88fef4330ddebd7a2b45d489ad9df5e102c91e
Neeraj Kumar Saroya <neeraj.saroya@hotmail.com>
2019-09-11 02:29:45 +0530

    Changed App.js

--- src/App.js
+++ src/App.js
@@ -11,7 +11,7 @@ class App extends Component {
           <h2>Welcome to React</h2>
         </div>
         <p className="App-intro">
-          Hi there Neer!
+          Hi there Neeraj Kumar Saroya!
         </p>
       </div>
     );
